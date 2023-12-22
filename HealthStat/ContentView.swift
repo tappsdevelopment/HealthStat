91f3d008ff5e6781dbf875fbe6dc4a1d93415887
Tobias Schweizer <schweizer.tobias@gmx.de>
2023-12-22 09:29:16 +0100

    Initial Commit

---
+++ HealthStat/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  HealthStat
+//
+//  Created by Tobias Schweizer on 22.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        //Content
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthStat/HealthStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthStatApp.swift
+//  HealthStat
+//
+//  Created by Tobias Schweizer on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct HealthStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
